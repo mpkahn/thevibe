2a68e3aca21b99ee3cfca4ce78cd87e04580cd0e
dustin.knowles <dustin.knowles22@gmail.com>
2021-01-28 19:45:44 -0800

    adding ajax function for pulling google books info

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
+let userInput = $("#user-input").val();
 
-$("#add-vibe").on("click", function(event) {
-
-event.preventDefault()
-console.log("hello")
-const userInput = $("#user-input").val();
-const queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + userInput + "&key=AIzaSyAIRa_UYE_tGr5zwxbcAlrStZrQRhOL9PE";
-
-console.log(queryURL);
-console.log(userInput);
+$("#add-vibe").on("click", function (event) {
+  userInput = $("#user-input").val();
+  event.preventDefault()
 
 
-$.ajax({
+  const queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + userInput + "&key=AIzaSyAIRa_UYE_tGr5zwxbcAlrStZrQRhOL9PE";
+
+  console.log(queryURL);
+  console.log(userInput);
+
+
+  $.ajax({
     url: queryURL,
     method: "GET"
   })
-    .then(function(response) {
+    .then(function (response) {
       console.log(response.items[0]);
-    const vidId = (response.items[0].id.videoId);
-    $("#vibe-form").append("www.youtube.com/watch?v=" + vidId);
+      const vidId = (response.items[0].id.videoId);
+      $("#vibe-form").append("www.youtube.com/watch?v=" + vidId);
 
     });
+
+  const queryURL2 = "https://books.googleapis.com/books/v1/volumes?q=" + userInput + "&key=AIzaSyBv3j_f2yZkDmSg1UdFZFtJT1v9rOIBudY"
+
+  $.ajax({
+    url: queryURL2,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(response)
+      console.log(response.items[0])
+    })
 });
+
+
+
