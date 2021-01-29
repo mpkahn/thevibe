562b7b68fab9ba07dae5f4e610eac670ecaa544b
Michael Kahn <kahn.mp@gmail.com>
2021-01-28 18:42:20 -0800

    updated youtube api js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-// $("search").on("click", function(event) {
-// console.log("hello")
-// event.preventDefault()
-// const userInput = $("query").val().trim();
-const queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=happy&key=AIzaSyAIRa_UYE_tGr5zwxbcAlrStZrQRhOL9PE";
+$("#add-vibe").on("click", function(event) {
+
+event.preventDefault()
+console.log("hello")
+const userInput = $("#user-input").val();
+const queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + userInput + "&key=AIzaSyAIRa_UYE_tGr5zwxbcAlrStZrQRhOL9PE";
 
 console.log(queryURL);
-// console.log(userInput);
+console.log(userInput);
 
 
 $.ajax({
@@ -14,7 +15,9 @@ $.ajax({
     method: "GET"
   })
     .then(function(response) {
-      console.log(response);
-     
+      console.log(response.items[0]);
+    const vidId = (response.items[0].id.videoId);
+    $("#vibe-form").append("www.youtube.com/watch?v=" + vidId);
+
     });
-// });
+});
